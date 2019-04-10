3426af1dc65f902f077a8517e4ba319789163fb6
hirock17l@gmail.com <hirock@hirocknoMac-mini.local>
2019-04-10 17:35:20 +0900

    dataの表示

--- todoapp/src/App.vue
+++ todoapp/src/App.vue
@@ -1,28 +1,27 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ul>
+      <li v-for="todo in todos">
+        <span>{{ todo.text }}</span>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      todos: [
+        { id: 1, text: 'dataを表示する' },
+        { id: 2, text: 'ディレクティブをしる' },
+        { id: 3, text: '追加テスト' }
+      ],
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>

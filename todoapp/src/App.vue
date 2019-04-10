56ddc8c59916d63f2ddf80f282cda8f6ce415285
hirock17l@gmail.com <hirock@hirocknoMac-mini.local>
2019-04-10 19:20:56 +0900

    todoの追加と削除機能の追加

--- todoapp/src/App.vue
+++ todoapp/src/App.vue
@@ -15,10 +15,20 @@ export default {
     return {
       todos: [
         { id: 1, text: 'dataを表示する' },
-        { id: 2, text: 'ディレクティブをしる' },
-        { id: 3, text: '追加テスト' }
+        { id: 2, text: 'ディレクティブをしる' }
       ],
+      newTodo: ''
     }
+  },
+  methods: {
+    addTodo: function() {
+      const newId = Math.max.apply(null, this.todos.map(t => t.id)) + 1;
+      this.todos.push({ id: newId, text: this.newTodo});
+      this.newTodo = '';
+    },
+    deleteTodo: function(todo) {
+      this.todos = this.todos.filter(item => item !== todo);
+    },
   }
 }
 </script>

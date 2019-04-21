07b262e229a2204f6d76c85a2f5bc81b7b460139
hirock17l@gmail.com <hirock@hirocknoMac-mini.local>
2019-04-21 11:43:15 +0900

    googlemap-api

--- rakuten-api/src/App.vue
+++ rakuten-api/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
 
-    <form>
+    <form style="width: 460px;">
       <el-form ref="searchFormData" :model="searchFormData" label-width="120px">
         <el-form-item label="Activity place">
           <el-input v-model="searchFormData.placeName"></el-input>
@@ -16,78 +16,75 @@
           </el-col>
         </el-form-item>
         <el-form-item>
-          <el-button type="primary" v-on:click="googleMap">Submit</el-button>
+          <el-button type="primary" v-on:click="initMap">Submit</el-button>
           <el-button>Cancel</el-button>
         </el-form-item>
       </el-form>
     </form>
 
-    <!-- 緯度•経度の確認用 -->
-    <p>緯度: {{searchFormData.lat}}</p>
-    <p>経度: {{searchFormData.lng}}</p>
+    <div id="map"></div>
+
+    <!-- 確認用 -->
+    <p>住所: {{ searchFormData.placeName }}</p>
+    <p>チェックイン: {{ searchFormData.checkInDate }}</p>
+    <p>チェックアウト: {{ searchFormData.checkOutDate }}</p>
+    <!-- <p>{{ apikey }}</p> -->
+    <p>緯度: {{mapData.lat}}</p>
+    <p>経度: {{mapData.lng}}</p>
 
   </div>
 </template>
-<script src="https://maps.googleapis.com/maps/api/js?key=process.env.VUE_APP_GOOGLE_MAP_API_KEY&callback=initMap" async defer></script>
+<script async defer src="https://maps.googleapis.com/maps/api/js?key=process.env.VUE_APP_GOOGLE_MAP_API_KEY&callback=initMap">
+</script>
 <script>
   export default {
     name: 'app',
-    mounted() {
-          this.geocoder = new google.maps.Geocoder();
-        },
     data() {
       return {
-        geocoder: {},
+        apikey: process.env.VUE_APP_GOOGLE_MAP_API_KEY,
         searchFormData: {
           placeName: '',
           checkInDate: '',
           checkOutDate: '',
         },
-        results: null,
-        keyword: '',
-        params: {
-          format: 'json',
-          checkinDate: searchFormData.checkInDate,
-          checkoutDate: searchFormData.checkOutDate,
-          latitude: searchFormData.lat,
-          longitude: searchFormData.lng,
-          applicationId: 1045443135407692478
+        mapData: {
+          address: '',
+          lat: '',
+          lng: ''
         }
       }
     },
     methods: {
-      mapSearch() {
-        geocoder.geocode({ address: searchFormData.placeName }, function (results, status) {
-          if (status === google.maps.GeocoderStatus.OK) {
-            console.log(results[0].geometry.location);
+        initMap: function() {
+          var target = document.getElementById('map'); //マップを表示する要素を指定
+          var address = '東京都新宿区西新宿2-8-1'; //住所を指定
+          var geocoder = new google.maps.Geocoder();
 
-            this.lat = results[0].geometry.location.lat();
-            this.lng = results[0].geometry.location.lng();
+          geocoder.geocode({ address: mapData.address }, function (results, status) {
+            if (status === 'OK' && results[0]) {
 
+              console.log(results[0].geometry.location);
 
-          } else {
-            alert('住所が正しくないか存在しません');
-            target.style.display = 'none';
-          }
+              var map = new google.maps.Map(target, {
+                center: results[0].geometry.location,
+                zoom: 18
+              });
+
+              var marker = new google.maps.Marker({
+                position: results[0].geometry.location,
+                map: map,
+                animation: google.maps.Animation.DROP
+              });
+
+            } else {
+              //住所が存在しない場合の処理
+              alert('住所が正しくないか存在しません。');
+              target.style.display = 'none';
+            }
+
         });
-      },
-      rakuten: function() {
-        var self = this;
-        axios.get('https://app.rakuten.co.jp/services/api/Travel/VacantHotelSearch/20170426', {params: this.params})
-          .then(function (res) {
-            self.results = res.data.items;
-          })
-          .catch(function (err) {
-            // エラー
-            console.log(err);
-          })
       }
+    }
   }
-}
-// googleMap()
-// rakuten()
+
 </script>
-
-<style>
-
-</style>

fac9019e5deea5e18571414062bf5f44076e3573
hirock17l@gmail.com <hirock@hirocknoMac-mini.local>
2019-04-22 20:56:08 +0900

    rakuten axios.get

--- rakuten/main.js
+++ rakuten/main.js
@@ -1,51 +1,60 @@
 new Vue({
   el: "#app",
-  data: {
-    searchFormData: {
-      placeName: '',
-      checkInDate: '',
-      checkOutDate: '',
-    },
-    address: "東京都新宿区西新宿2-8-1",
-    lat: '',
-    lng: ''
+  data() {
+    return {
+      searchFormData: {
+        placeName: '',
+        checkInDate: '',
+        checkOutDate: '',
+      },
+      results: null,
+      lat: '',
+      lng: ''
+    }
   },
   methods: {
     search() {
-      // var address = this.searchFormData.placeName; //住所を指定
+      var address = this.searchFormData.placeName; //住所を指定
       // var address = "東京都新宿区西新宿2-8-1";//住所を指定
       // var target = document.getElementById('map');
       var map = new google.maps.Map(document.getElementById('map'));
       var geocoder = new google.maps.Geocoder();
 
-      geocoder.geocode({ address: this.searchFormData.placeName}, function (results, status) {
+      geocoder.geocode({ address: address }, function (results, status) {
         if (status === 'OK' && results[0]) {
           this.lat = results[0].geometry.location.lat();
           this.lng = results[0].geometry.location.lng();
+          console.log(this.checkinDate);
+          console.log(this.searchFormData.checkoutDate);
           // console.log(this.lat);
           // console.log(this.lng);
           // rakuten(this.lat, this.lng);
-
         } else {
-          //住所が存在しない場合の処理
           alert('住所が正しくないか存在しません。');
-
         }
-
       });
 
-      
-    },
-    rakuten: function () {
-      this.initMap();
-      axios.get('https://app.rakuten.co.jp/services/api/Travel/VacantHotelSearch/20170426', { params: this.params })
+      axios.get('https://app.rakuten.co.jp/services/api/Travel/VacantHotelSearch/20170426?', {
+        params: {
+          applicationId: 1023196580476476311,
+          format: 'JSON',
+          checkinDate: this.searchFormData.checkInDate,
+          checkoutDate: this.searchFormData.checkoutDate,
+          latitude: this.lat,
+          longitude: this.lng,
+          searchRadius: 1
+        }
+      })
         .then(function (res) {
-          self.results = res.data.items;
+          console.log(res);
         })
         .catch(function (err) {
           // エラー
           console.log(err);
         })
-      }
+
+
+
+    },
   }
 })

66ab07f3154797863b18b202e29911948437b06a
hirock17l@gmail.com <hirock@hirocknoMac-mini.local>
2019-04-23 23:46:54 +0900

    Map of undefined error

--- rakuten-api/src/App.vue
+++ rakuten-api/src/App.vue
@@ -1,101 +1,93 @@
 <template>
-  <div id="app">
-
-    <form style="width: 460px;">
-      <el-form ref="searchFormData" :model="searchFormData" label-width="120px">
-        <el-form-item label="Activity place">
-          <el-input v-model="searchFormData.placeName"></el-input>
-        </el-form-item>
-        <el-form-item label="Activity time">
-          <el-col :span="11">
-            <el-date-picker type="date" placeholder="Pick a date" v-model="searchFormData.checkInDate" style="width: 100%;"></el-date-picker>
-          </el-col>
-          <el-col class="line" :span="2">-</el-col>
-          <el-col :span="11">
-            <el-date-picker type="date" placeholder="Pick a date" v-model="searchFormData.checkOutDate" style="width: 100%;"></el-date-picker>
-          </el-col>
-        </el-form-item>
-        <el-form-item>
-          <el-button type="primary" v-on:click="initMap">Submit</el-button>
-          <el-button>Cancel</el-button>
-        </el-form-item>
-      </el-form>
-    </form>
+  <div id="app" class="wrapper">
+    <div id="content">
+      <form style="width: 460px;">
+          <el-form ref="searchFormData" :model="searchFormData" label-width="120px">
+            <el-form-item label="Activity place">
+              <el-input v-model="searchFormData.placeName"></el-input>
+            </el-form-item>
+            <el-form-item label="Activity time">
+              <el-col :span="11">
+                <el-date-picker type="date" placeholder="Pick a date" v-model="searchFormData.checkInDate" style="width: 100%;"></el-date-picker>
+              </el-col>
+              <el-col class="line" :span="2">-</el-col>
+              <el-col :span="11">
+                <el-date-picker type="date" placeholder="Pick a date" v-model="searchFormData.checkOutDate" style="width: 100%;"></el-date-picker>
+              </el-col>
+            </el-form-item>
+            <el-form-item>
+              <el-button type="primary" v-on:click="search">Submit</el-button>
+              <el-button>Cancel</el-button>
+            </el-form-item>
+          </el-form>
+        </form>
+      </div>
 
     <div id="map"></div>
-
     <!-- 確認用 -->
     <p>住所: {{ searchFormData.placeName }}</p>
     <p>チェックイン: {{ searchFormData.checkInDate }}</p>
     <p>チェックアウト: {{ searchFormData.checkOutDate }}</p>
     <!-- <p>{{ apikey }}</p> -->
-    <p>緯度: {{mapData.lat}}</p>
-    <p>経度: {{mapData.lng}}</p>
+    <p id="lat">緯度: {{ lat }}</p>
+    <p id="lng">経度: {{ lng }}</p>
 
   </div>
+
 </template>
-<script async defer src="https://maps.googleapis.com/maps/api/js?key=process.env.VUE_APP_GOOGLE_MAP_API_KEY&callback=initMap">
-</script>
+
 <script>
   export default {
     name: 'app',
     data() {
       return {
-        apikey: process.env.VUE_APP_GOOGLE_MAP_API_KEY,
         searchFormData: {
           placeName: '',
           checkInDate: '',
           checkOutDate: '',
         },
-        mapData: {
-          address: '',
-          lat: '',
-          lng: ''
-        },
-        // params: {
-        //   format: 'json',
-        //   checkinDate: searchFormData.checkInDate,
-        //   checkoutDate: searchFormData.checkOutDate,
-        //   latitude: searchFormData.lat,
-        //   longitude: searchFormData.lng,
-        //   applicationId: 1045443135407692478
-        // }
+        results: null,
+        map: null,
+        mapPosition: { lat: 35.658034, lng: 139.701636 },
+        geocoder: {},
+        lat: '',
+        lng: '',
+        params: {
+          applicationId: '1023196580476476311',
+          format: 'JSON',
+          latitude: this.lat,
+          longitude: this.lng,
+          searchRadius: '1'
+        }
       }
     },
     methods: {
-        initMap: function() {
-          var address = this.placeName ; //住所を指定
-
-          var geocoder = new google.maps.Geocoder();
-
-          geocoder.geocode({ address: mapData.address }, function (results, status) {
-            if (status === 'OK' && results[0]) {
-
-              // console.log(results[0].geometry.location);
-
-              mapData.lat = results[0].geometry.location.lat();
-              mapData.lng = results[0].geometry.location.lng();
-
-            } else {
-              //住所が存在しない場合の処理
-              alert('住所が正しくないか存在しません。');
-              target.style.display = 'none';
-            }
-
-          });
-        },
-        // rakuten: function () {
-        // var self = this;
-        // axios.get('https://app.rakuten.co.jp/services/api/Travel/VacantHotelSearch/20170426', { params: this.params })
-        //   .then(function (res) {
-        //     self.results = res.data.items;
-        //   })
-        //   .catch(function (err) {
-        //     // エラー
-        //     console.log(err);
-        //   })
-        // }
-      }
+      search: function () {
+        this.map = new this.VueGoogleMaps.Map(document.getElementById('map'));
+        this.geocoder = new this.VueGoogleMaps.Geocoder();
+        this.geocoder.geocode({ address: this.searchFormData.placeName }, function (results, status) {
+          if (status === 'OK' && results[0]) {
+            this.lat = results[0].geometry.location.lat();
+            this.lng = results[0].geometry.location.lng();
+          } else {
+            //住所が存在しない場合の処理
+            alert('住所が正しくないか存在しません。');
+          }
+        })
+      },
+      // rakuten: function () {
+      //   axios.get('https://app.rakuten.co.jp/services/api/Travel/VacantHotelSearch/20170426?', { params: this.params })
+      //     .then(function (res) {
+      //       // 処理
+      //       console.log(res);
+      //     })
+      //     .catch(function (err) {
+      //       // エラー
+      //       console.log(err);
+      //     })
+      // }
+    }
   }
+
 
 </script>
--- rakuten-api/src/main.js
+++ rakuten-api/src/main.js
@@ -1,11 +1,24 @@
 import Vue from 'vue'
+import App from './App.vue'
+import * as VueGoogleMaps from 'vue2-google-maps'
+
+
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-import App from './App.vue'
+// import axios from 'axios'
+// import Search from './components/Search.vue'
 
 Vue.use(ElementUI)
 Vue.config.productionTip = false
 
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: process.env.VUE_APP_GOOGLE_MAP_API_KEY,
+  },
+  installComponents: false,
+});
+
 new Vue({
+  // router,
   render: h => h(App),
 }).$mount('#app')
